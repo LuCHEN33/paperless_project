cc731b21757b5752b04c0da54c3d268be682edc9
LucatChen <xl.chen@protonmail.com>
2023-12-30 19:29:22 +0100

    init

---
+++ paperless/src/test/java/at/fhtw/swen3/paperless/PaperlessProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.swen3.paperless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperlessProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
