349a9a9ea9fa9eac2fcaaba5d6969e8d66d63b3c
LucatChen <xl.chen@protonmail.com>
2024-01-14 16:50:49 +0100

    Revert "generated ui and api server"
    This reverts commit 37c0e200c3486bc0bffce50242564d1a82dbb573.

---
+++ paperless/src/main/java/at/fhtw/swen3/paperless/PaperlessProjectApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.swen3.paperless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperlessProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperlessProjectApplication.class, args);
+	}
+
+}
