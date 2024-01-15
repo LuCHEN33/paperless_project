4083b1be95046c456f725dae8c754ece0d8a70f2
LucatChen <xl.chen@protonmail.com>
2024-01-15 11:29:12 +0100

    new generated

---
+++ src/main/java/com/paperless/api/PaperlessApiController.java
@@ -0,0 +1,34 @@
+package com.paperless.api;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import javax.annotation.Generated;
+import java.util.Optional;
+
+
+@Generated(value = "com.paperless.codegen.languages.SpringCodegen", date = "2023-10-22T12:32:07.613318Z[Etc/UTC]")
+@Controller
+@RequestMapping("${openapi.paperlessRestServer.base-path:}")
+public class PaperlessApiController implements PaperlessApi {
+
+
+    private final NativeWebRequest request;
+
+    @Autowired
+    public PaperlessApiController(NativeWebRequest request) {
+        this.request = request;
+
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+
+}
