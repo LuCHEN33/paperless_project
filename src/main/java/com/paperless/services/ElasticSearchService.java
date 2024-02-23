1c5ce4e40294cbfc52d20edd62b38d2b3c8cf5ea
adrian <81982963+jizyo@users.noreply.github.com>
2024-02-23 10:57:12 +0100

    added indexDocument and getDocumentById

--- src/main/java/com/paperless/services/ElasticSearchService.java
+++ src/main/java/com/paperless/services/ElasticSearchService.java
@@ -2,6 +2,8 @@ package com.paperless.services;
 
 import co.elastic.clients.elasticsearch._types.Result;
 import com.paperless.persistence.entities.DocumentsDocumentEntity;
+import com.paperless.services.dto.DocumentDTO;
+
 
 import java.io.IOException;
 import java.util.List;
@@ -10,4 +12,8 @@ import java.util.Optional;
 public interface ElasticSearchService {
 
     List<DocumentsDocumentEntity> findDocs(String searchText) throws IOException ;
+
+    Result indexDocument(DocumentDTO documentDTO) throws IOException;
+
+    Optional<DocumentDTO> getDocumentById(int id);
 }
