cc7c1c1602dc5cc5ca02a074eeb69842afbb9f07
adrian <81982963+jizyo@users.noreply.github.com>
2024-02-23 10:57:47 +0100

    added indexDocument and getDocumentById

--- src/main/java/com/paperless/services/ElasticSearchServiceImpl.java
+++ src/main/java/com/paperless/services/ElasticSearchServiceImpl.java
@@ -1,11 +1,16 @@
 package com.paperless.services;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.Result;
+import co.elastic.clients.elasticsearch.core.GetResponse;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.search.Hit;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
+import com.paperless.configuration.ElasticSearchConfig;
 import com.paperless.persistence.entities.DocumentsDocumentEntity;
 import com.paperless.persistence.repositories.DocumentsDocumentRepository;
+import com.paperless.services.dto.DocumentDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
@@ -62,6 +67,44 @@ public class ElasticSearchServiceImpl implements ElasticSearchService {
 
         return resultEntities;
     }
+    @Override
+    public Result indexDocument(DocumentDTO documentDTO) {
+        // do indexing with ElasticSearch
+        IndexResponse response = null;
+        try {
+            response = esClient.index(i -> i
+                    .index(ElasticSearchConfig.DOCUMENTS_INDEX_NAME)
+                    .id(String.valueOf(documentDTO.getId()))
+                    .document(documentDTO)
+            );
+        } catch (IOException e) {
+            log.error("Error indexing document \n" + e.getMessage());
+        }
+        assert response != null;
+
+        String logMsg = "Indexed document " + documentDTO.getId() + documentDTO.getTitle() + ": result=" + response.result() + ", index=" + response.index();
+        if ( response.result()!=Result.Created && response.result()!=Result.Updated )
+            log.error("Failed to " + logMsg);
+        else
+            log.info(logMsg);
+        return response.result();
+    }
+
+    @Override
+    public Optional<DocumentDTO> getDocumentById(int id) {
+        try {
+            GetResponse<DocumentDTO> response = esClient.get(g -> g
+                            .index(ElasticSearchConfig.DOCUMENTS_INDEX_NAME)
+                            .id(String.valueOf(id)),
+                    DocumentDTO.class
+            );
+            return (response.found() && response.source()!=null) ? Optional.of(response.source()) : Optional.empty();
+        } catch (IOException e) {
+            log.error("Failed to get document id=" + id + " from elasticsearch: " + e);
+            return Optional.empty();
+        }
+    }
+
 }
 
 
