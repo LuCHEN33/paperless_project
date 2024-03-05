528d977ca7d26aad803c73209b62f46c272069ac
LucatChen <xl.chen@protonmail.com>
2024-03-05 21:17:03 +0100

    add MinIOConfig

---
+++ OcrService/tesseract/src/main/java/com/paperless/tesseract/configuration/MinIOConfig.java
@@ -0,0 +1,46 @@
+package com.paperless.tesseract.configuration;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinIOConfig {
+
+    @Value("${minio.url}")
+    private String url;
+
+    @Value("${minio.access.name}")
+    private String accessKey;
+
+    @Value("${minio.access.secret}")
+    private String accessSecret;
+
+    @Value("${minio.bucketName}")
+    private String bucketName;
+
+
+    @Bean
+    public MinioClient MinioClient() {
+        try {
+            MinioClient minioClient = MinioClient.builder()
+                    .endpoint(url)
+                    .credentials(accessKey, accessSecret)
+                    .build();
+
+            // Check if the bucket already exists
+            boolean isExist = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+            if (!isExist) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+            }
+
+            return minioClient;
+        } catch (Exception e) {
+            throw new RuntimeException("Could not init MinioClient", e);
+        }
+    }
+
+}
