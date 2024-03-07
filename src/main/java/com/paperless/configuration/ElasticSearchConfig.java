a5166703755300a2b41482d0eaf8a402831950f7
LucatChen <xl.chen@protonmail.com>
2024-03-07 12:32:22 +0100

    add ElasticSearchConfig

---
+++ src/main/java/com/paperless/configuration/ElasticSearchConfig.java
@@ -0,0 +1,71 @@
+package com.paperless.configuration;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RestClientBuilder;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticSearchConfig
+{
+
+    @Value("${elasticsearch.host}")
+    private String host;
+    @Value("${elasticsearch.port}")
+    private int port;
+
+
+    @Value("${elasticsearch.username}")
+    private String username;
+
+    @Value("${elasticsearch.password}")
+    private String password;
+
+    final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+
+
+    @Bean
+    public RestClient restClient() {
+        final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        return RestClient.builder(new HttpHost(host, port))
+                .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
+                .setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder.setSocketTimeout(10000))
+                .build();
+    }
+
+    @Bean
+    public ElasticsearchTransport getElasticsearchTransport() {
+        JacksonJsonpMapper mapper = new JacksonJsonpMapper();
+        mapper.objectMapper().registerModule(new JavaTimeModule());
+        mapper.objectMapper().registerModule(new JsonNullableModule());
+        return new RestClientTransport(
+                restClient(), mapper);
+    }
+
+
+    @Bean
+    public ElasticsearchClient getElasticsearchClient(){
+        return new ElasticsearchClient(getElasticsearchTransport());
+    }
+
+}
